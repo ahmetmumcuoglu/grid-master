546eeb922163d0bc824fc02bab1fd354786689af
ahmetmumcuoglu <128048095+ahmetmumcuoglu@users.noreply.github.com>
2026-03-13 15:21:10 +0300

    Update config.js

--- tr/config.js
+++ tr/config.js
@@ -1,1 +1,10 @@
-
+// Firebase Configuration - Grid Master Pro
+export const firebaseConfig = {
+    apiKey: "AIzaSyB-hCbDg4JCRhN1TU6P9oCuzYlxaAcGGAY",
+    authDomain: "grid-master-ac112.firebaseapp.com",
+    projectId: "grid-master-ac112",
+    storageBucket: "grid-master-ac112.firebasestorage.app",
+    messagingSenderId: "39631696882",
+    appId: "1:39631696882:web:2bf956e7f6d9123a4a316b",
+    measurementId: "G-QQ36P4DVTD"
+};
